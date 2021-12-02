7a7893b13db0af878d5eea1986762960a92146e6
GuiQRSM <endbeob15@gmail.com>
2021-12-02 15:16:55 -0300

    Calls within the Scaffold() returned by the stful structure, the body attribute containing the Container() class where width is set and within this Container class it calls the SingleChildScrollView() class as child which configures the general padding and receives the Column() class where the Widgets will be implemented within its children attribute.

--- lib/InicialFrame.dart
+++ lib/InicialFrame.dart
@@ -8,15 +8,29 @@ class InicialFrame extends StatefulWidget {
 }
 
 var mainColor = Color.fromRGBO(43, 120, 62, 1);
-var supportingColor = Color.fromRGBO(54, 201, 88, 1);
+var supportingColor = Color.fromRGBO(122, 220, 145, 1);
 
 class _InicialFrameState extends State<InicialFrame> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ATM Consultoria"),
+        title: Text("ATM Consultoria",
+        style: TextStyle(
+          color: supportingColor,
+        ),),
         backgroundColor: mainColor,
+      ),
+      body: Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+
+            ],
+          ),
+        ),
       ),
     );
   }
