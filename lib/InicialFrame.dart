55a608a82ccf8a12fe82a0bf62117d6e27aa542d
GuiQRSM <endbeob15@gmail.com>
2021-12-02 15:09:08 -0300

    It implements the mainColor and supportingColor variables to set colors and calls the appBar attribute inside the Scaffold() class returned by stful.

--- lib/InicialFrame.dart
+++ lib/InicialFrame.dart
@@ -7,9 +7,17 @@ class InicialFrame extends StatefulWidget {
   _InicialFrameState createState() => _InicialFrameState();
 }
 
+var mainColor = Color.fromRGBO(43, 120, 62, 1);
+var supportingColor = Color.fromRGBO(54, 201, 88, 1);
+
 class _InicialFrameState extends State<InicialFrame> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ATM Consultoria"),
+        backgroundColor: mainColor,
+      ),
+    );
   }
 }
