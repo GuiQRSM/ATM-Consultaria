5db06d8fc54aa59098f4ab9be1284cff51833ee8
GuiQRSM <endbeob15@gmail.com>
2021-12-02 14:58:13 -0300

    Creates in the lib folder a dart file named InitialFrame with the stful structure returning a Scaffold(), already implemented.

---
+++ lib/InicialFrame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InicialFrame extends StatefulWidget {
+  const InicialFrame({Key? key}) : super(key: key);
+
+  @override
+  _InicialFrameState createState() => _InicialFrameState();
+}
+
+class _InicialFrameState extends State<InicialFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
