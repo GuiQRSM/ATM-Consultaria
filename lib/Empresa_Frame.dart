1b8bbbcefe8b930c3b0db2050b0682c67449c6ac
GuiQRSM <endbeob15@gmail.com>
2021-12-03 14:40:25 -0300

    Creates in the lib folder a new dart file called 'Empresa_Frame' and already defines the material.dart import and the stful structure with the class 'EmpresaFrame' and returning a Scaffold().

---
+++ lib/Empresa_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmpresaFrame extends StatefulWidget {
+  const EmpresaFrame({Key? key}) : super(key: key);
+
+  @override
+  _EmpresaFrameState createState() => _EmpresaFrameState();
+}
+
+class _EmpresaFrameState extends State<EmpresaFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
