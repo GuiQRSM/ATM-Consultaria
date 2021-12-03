3cf99ecd15345017f4f8fce0e6c7a692ed25b4e1
GuiQRSM <endbeob15@gmail.com>
2021-12-03 14:50:14 -0300

    Creates in the lib folder a new dart file called 'Contato_Frame' and already defines the import of material.dart and the stful structure with the class 'ContatoFrame' and returning a Scaffold().

---
+++ lib/Contato_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContatoFrame extends StatefulWidget {
+  const ContatoFrame({Key? key}) : super(key: key);
+
+  @override
+  _ContatoFrameState createState() => _ContatoFrameState();
+}
+
+class _ContatoFrameState extends State<ContatoFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
