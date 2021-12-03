c58b722f4263138b0d392acdf772b7fa7a710a65
GuiQRSM <endbeob15@gmail.com>
2021-12-03 14:43:19 -0300

    Creates in the lib folder a new dart file called 'Servico_Frame' and already defines the material.dart import and the stful structure with the class 'ServicoFrame' and returning a Scaffold().

---
+++ lib/Servico_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ServicoFrame extends StatefulWidget {
+  const ServicoFrame({Key? key}) : super(key: key);
+
+  @override
+  _ServicoFrameState createState() => _ServicoFrameState();
+}
+
+class _ServicoFrameState extends State<ServicoFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
