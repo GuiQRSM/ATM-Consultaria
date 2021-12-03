485da74bc2e2d9fa7cfd590ecfc1cce4d2353a25
GuiQRSM <endbeob15@gmail.com>
2021-12-03 15:29:26 -0300

    It declares in the Scaffold() class that the body attribute is returned from the stful structure of 'CompanyFrame', which contains the Container() class which as child SingleChildScrollView() which has as child the Column() class which contains a 'children: <Widget >[]' where the Widgets are located.

--- lib/Empresa_Frame.dart
+++ lib/Empresa_Frame.dart
@@ -21,6 +21,41 @@ class _EmpresaFrameState extends State<EmpresaFrame> {
           ),),
         backgroundColor: mainColor,
       ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(18),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Image.asset(
+                        "imgs/detalhe_empresa.png",
+                    height: 80,
+                    ),
+                    Text(
+                        "Sobre a Empresa",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: mainColor,
+                    ),),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 14),
+                child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
