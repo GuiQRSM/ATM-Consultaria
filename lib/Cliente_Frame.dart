5742cf63e7e9d35c9eed63397bdc9db6d6e30017
GuiQRSM <endbeob15@gmail.com>
2021-12-03 14:48:25 -0300

    Creates a new dart file in the lib folder called 'Cliente_Frame' and already defines the import of material.dart and the stful structure with the class 'ClientFrame' and returning a Scaffold().

---
+++ lib/Cliente_Frame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClienteFrame extends StatefulWidget {
+  const ClienteFrame({Key? key}) : super(key: key);
+
+  @override
+  _ClienteFrameState createState() => _ClienteFrameState();
+}
+
+class _ClienteFrameState extends State<ClienteFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
